1590f3ff66cb086e41a9bcc57b8eb76df021328b
frank <zhy20090912@gmail.com>
2017-07-26 11:23:47 +0000

    init commit

---
+++ README.md
@@ -0,0 +1,16 @@
+```bash
+# .bashrc
+export FLASK_APP=/path/to/app.py
+export FLASK_DEBUG=1  #debug mode, auto reload when source are changed and other features
+```
+source ~/.bashrc
+flask run --port=0.0.0.0
+
+```
+ * Serving Flask app "app"
+ * Forcing debug mode on
+ * Running on http://0.0.0.0:5000/ (Press CTRL+C to quit)
+ * Restarting with stat
+ * Debugger is active!
+ * Debugger PIN: 287-336-933
+```
---
+++ app.py
@@ -0,0 +1,53 @@
+from flask import Flask, jsonify, session, redirect, url_for, escape,make_response, request
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+  if 'username' in session:
+    username = session['username']
+    resp = make_response(jsonify(
+      user_name=username
+    ))
+    return resp
+  return 'You are not logged in'
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+  if request.method == 'POST':
+    session['username'] = request.form['username']
+    return redirect(url_for('index'))
+  return '''
+    <form method="post">
+      <p><input type=text name=username>
+      <p><input type=submit value=Login>
+    </form>
+  '''
+
+@app.route('/logout')
+def logout():
+  # remove the username from the session if it's there
+  session.pop('username', None)
+  return redirect(url_for('index'))
+
+@app.route('/json')
+def return_json():
+  resp = make_response(jsonify(
+    id=12,
+    user_name="username",
+    email='email'
+  ))
+  app.logger.debug('A value for debugging')
+  app.logger.warning('A warning occurred (%d apples)', 42)
+  app.logger.error('An error occurred')
+  resp.set_cookie('id', '123')
+  resp.set_cookie('value', 'value', httponly=False)
+  return resp
+
+@app.errorhandler(404)
+def page_not_found(error):
+  return render_template('page_not_found.html'), 404
+
+# set the secret key.  keep this really secret:
+app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
